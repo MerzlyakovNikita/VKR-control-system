e56e05dae214b579a25436b867b2287fd80607f3
Nikita Merzlyakov <nik.merzlyakov.2003@mail.ru>
2026-03-30 20:58:55 +0500

    feat: добавил подключение к БД PostgreSQL

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const cors = require('cors')
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`)
